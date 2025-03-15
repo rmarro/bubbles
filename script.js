e4b1eca95b5aaef7e2652e8a993d8826d4837240
Rachel Marro <30510953+rmarro@users.noreply.github.com>
2025-03-15 11:33:01 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 const gameContainer = document.getElementById('game-container');
 gameContainer.dataset.level = 0;
+let popAction = document.getElementById('pop-action').value;
+
+function setPopAction(value) {
+    popAction = value;
+}
 
 function setStyle(classes) {
-    const classesArray = classes.split(" ")
-    gameContainer.classList.remove(...['filled','rainbow','earth','air','fire','water'])
-    gameContainer.classList.add(...classesArray)
+    const classesArray = classes.split(" ");
+    gameContainer.classList.remove(...['rainbow','spa-1','spa-2','spa-3','spa-4','earth','air','fire','water']);
+    gameContainer.classList.add(...classesArray);
+}
+
+function setFilled(value) {
+    if (value) {
+        gameContainer.classList.add('filled');
+    } else {
+        gameContainer.classList.remove('filled');
+    }
 }
 
 function initializeGrid (value) {
-    gameContainer.innerHTML = ''
+    gameContainer.innerHTML = '';
     createGrid(gameContainer, value);
 }
 
@@ -21,22 +34,33 @@ function createGrid(parentCell, rows=null) {
         cell.classList.add(`bubble-${newLevel}`);
         cell.classList.add('cell');
         cell.dataset.level = newLevel;
-        cell.addEventListener('click', handleCellClick);
+        cell.addEventListener('click', handleCellInteraction);
+        cell.addEventListener('mouseover', handleCellInteraction);
         parentCell.appendChild(cell);
     }
 }
 
-function handleCellClick (event) {
+function handleCellInteraction (event) {
     const cell = event.target;
     const level = cell.dataset.level;
+    const ignore = (event.type != popAction);
 
-    if (level === '6') {
+    if (ignore || level === '6') {
         return
     }
 
-    cell.classList.remove(`bubble-${level}`);
-    cell.removeEventListener('click', handleCellClick)
-    createGrid(cell)
+    removeListeners(cell);
+    const timeout = (popAction === 'click') ? 0 : 25;
+
+    setTimeout(() => {
+        cell.classList.remove(`bubble-${level}`);
+        createGrid(cell)
+    }, timeout);
+}
+
+function removeListeners(cell) {
+    cell.removeEventListener('click', handleCellInteraction);
+    cell.removeEventListener('mouseover', handleCellInteraction);
 }
 
 createGrid(gameContainer, 2)
