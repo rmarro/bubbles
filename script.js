66869f956b5bbf82b485be1553f2a8d9c48a90c4
Rachel Marro <30510953+rmarro@users.noreply.github.com>
2025-03-15 00:08:30 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 const gameContainer = document.getElementById('game-container');
 gameContainer.dataset.level = 0;
 
-function createGrid(parentCell) {
+function setStyle(classes) {
+    const classesArray = classes.split(" ")
+    gameContainer.classList.remove(...['filled','rainbow','earth','air','fire','water'])
+    gameContainer.classList.add(...classesArray)
+}
+
+function initializeGrid (value) {
+    gameContainer.innerHTML = ''
+    createGrid(gameContainer, value);
+}
+
+function createGrid(parentCell, rows=null) {
+    rows ||= 2
     const newLevel = parseInt(parentCell.dataset.level) + 1
 
-    for (let i = 0; i < 4; i++) {
+    for (let i = 0; i < rows * 2; i++) {
         const cell = document.createElement('div');
         cell.classList.add(`bubble-${newLevel}`);
+        cell.classList.add('cell');
         cell.dataset.level = newLevel;
         cell.addEventListener('click', handleCellClick);
         parentCell.appendChild(cell);
@@ -26,4 +39,5 @@ function handleCellClick (event) {
     createGrid(cell)
 }
 
-createGrid(gameContainer)
+createGrid(gameContainer, 2)
+setStyle("rainbow")
